abbc21e9725a4bc789196a6927236f0c6fd62f05
lorszil <lorszil@gmail.com>
2016-05-17 11:34:08 +0200

    Add files via upload

---
+++ 100doors.py
@@ -0,0 +1,26 @@
+import sys
+
+#j=doors count
+#i=ciklus
+
+doors = [0] * 101
+
+def switch(x):
+	global doors
+	if doors[x] == 0:
+		doors[x] = 1
+	else :
+		doors[x] = 0
+
+for i in range(1,101):
+	for j in range(1,101):
+		if j%i == 0:
+			switch(j)
+			j += 1
+		else:
+			j += 1
+	i += 1
+
+for y in range (1,101):
+	if doors[y] == 1:
+		print(y)
